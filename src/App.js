adef0631b3f6e50e7c14382e35f3cdd313b52ec9
chimseryOo001 <chimsemgr001@gmail.com>
2017-07-13 17:49:12 +0545

    Practice change

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
   render() {
-    const helloWorld = 'Welcome to Hackernoon Clone';
-
-    function formatName(user) {
-      return user.firstName + ' ' + user.lastName;
-    }
-
-    const user = {
-      "firstName": "Aakash",
-      "lastName": "Thapa",
-    };
+    const list = [
+      {
+        title: 'React',
+        url: 'https://facebook.github.io/react/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0
+      },
+      {
+        title: 'Redux',
+        url: 'https://github.com/reactjs/redux',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1
+      }
+    ];
 
     return (
-      <div className="App">                  
-        <h1>{helloWorld}</h1>
-        <h2>Who is doing this?</h2>
-        <h4>This is being done by {formatName(user)}!</h4>
+      <div className="App">
+        {list.map(function(item) {
+          return (
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>
+                  {item.title}
+                </a>
+              </span>
+              <span>
+                {item.author}
+              </span>
+              <span>
+                {item.num_comments}
+              </span>
+              <span>
+                {item.points}
+              </span>
+            </div>
+          );
+        })}
       </div>
     );
   }
